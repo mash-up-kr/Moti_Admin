2d48bbe96db42d7ea1dc9af0a0b4feaaf3d5b9f2
kor-Chipmunk <rhj4862@gmail.com>
2021-02-03 01:21:10 +0900

    :sparkles: Admin URL Root 주소로 옮김

--- app/urls.py
+++ app/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('/', admin.site.urls),
+    url(r'^', admin.site.urls),
     path('files/', include('files.urls')),
     path('questions/', include('questions.urls')),
     path('missions/', include('missions.urls')),
